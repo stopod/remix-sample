0c073647808494ac5df5fe2159d7881a53d462c5
ogo-js <taichi.ogo@japan-systems.co.jp>
2024-07-25 01:03:35 +0900

    feat: add storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,15 @@
+import "../app/tailwind.css";
+import type { Preview } from "@storybook/react";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
